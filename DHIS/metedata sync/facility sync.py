1a06db3009ce7740049aa8222b10e572a9f5958c
bmugwe <bonfacemugweh@gmail.com>
2025-06-23 16:27:21 +0300

    added create indicators programatically

--- DHIS/metedata sync/facility sync.py
+++ DHIS/metedata sync/facility sync.py
@@ -9,19 +9,25 @@ import certifi
 
 timenow = datetime.now().strftime("%Y-%m-%d %H:%M")
 
+# username = os.getenv("KHIS_USERNAME", "")
+# password = os.getenv("KHIS_PASSWORD", "")
+
+
 username = os.getenv("KHIS_USERNAME", "")
 password = os.getenv("KHIS_PASSWORD", "")
+
+
 
 print(f" Username: {username} - Password: {password}")
 domain_name = "hiskenya.org"
 
 post_systems = [
-    {
-        "name": "JPHES",
-        "url": "jphesportal.uonbi.ac.ke",
-        "username": "JPHES_USERNAME",
-        "password": "JPHES_PASSWORD",
-    }
+    # {
+    #     "name": "JPHES",
+    #     "url": "jphesportal.uonbi.ac.ke",
+    #     "username": "JPHES_USERNAME",
+    #     "password": "JPHES_PASSWORD",
+    # }
     # ,
     # {
     #     "name": "PPMS",
@@ -30,12 +36,12 @@ post_systems = [
     #     "password": "ppms_password",
     # }
     # ,
-    # {
-    #     "name": "Histracker",
-    #     "url": "histracker.health.go.ke",
-    #     "username": "Tracker_username",
-    #     "password": "Tracker_password",
-    # }
+    {
+        "name": "Histracker",
+        "url": "histracker.health.go.ke",
+        "username": "",
+        "password": "",
+    }
     # ,
     # {
     #     "name": "GBV",
@@ -45,67 +51,46 @@ post_systems = [
     # }
     # ,
     # {
-    #     "name": "Play Instance",
-    #     "url": "play.im.dhis2.org/stable-2-40-5",
-    #     "username": "",
-    #     "password": "",
+    #     "name": "Entomology Database",
+    #     "url": "ento.uonbi.ac.ke",
+    #     "username": "Ento_user",
+    #     "password": "Ento_password",
+    # }
+    # ,
+    # {
+    #     "name": "Bush",
+    #     "url": "43.205.46.180:8080",
+    #     "username": "admin",
+    #     "password": "district",
+    # },
+    # {
+    #     "name": "EBSME",
+    #     "url": "dhis.atsl.co.ke",
+    #     "username": "admin",
+    #     "password": "123456@Ab",
     # },
 ]
 
 
-credentials = f"{username}:{password}"
+credentials = f"{username}:{password}" 
 auth_coded = base64.b64encode(credentials.encode()).decode("utf-8")
+
+credentials_2 = ":" 
+auth_coded_2 = base64.b64encode(credentials_2.encode()).decode("utf-8")
 
 
 def fetchNewFacilities():
     pass
 
 
-facilities = [
-   'TCdh15iUFXg',
-'PEb8BZW2ziu',
-'BDuZCU59CxZ',
-'is8Ufsq3KGl',
-'prE5hsoV8H6',
-'lmR9ogNtSEc',
-'UlRf4kjQmSx',
-'fBecBcyGAzh',
-'ildgwkoH4Ao',
-'pMXH6YIN9RO',
-'FIcdZgZRBEN',
-'BCaEfrQW9LL',
-'TBA5r7aCUN5',
-'QxwoBAZjFtC',
-'UaVCwwsTtPy',
-'N9Np8tQvoG2',
-'zdqrcIoHhzb',
-'eUaWT4B0dhy',
-'mlHL2HP7LD0',
-'czTiQzpJBm0',
-'rSbnOkxQ4Qq',
-'K0Kj5Y0NQwO',
-'QLUqB0z21XE',
-'UdVgB8pVaKd',
-'VQ7GShr4P1l',
-'GUowNhmKNvt',
-'kov4giRgD4e',
-'NkDiME62bAG',
-'grlyT4w5ZSG',
-'JUBPnE7M0he',
-'gqxrvUoLoOo',
-'VZQhOoop4Ot',
-'HZNucg3esnL',
-'c2RfQcsfAEL',
-'Z0KyOqEohxa',
-'s3ly3dAGBf1',
-'gbkBDAbFArf',
-'ZYoasZOy2YF',
-'fcIgUtcR8Nm',
-'tryqC8rqyAN'
-]
+facilities = ["HfVjCurKxh2"]
 
 # facilities = fetchNewFacilities()
-base_url = "https://hiskenya.org/api/29/organisationUnits/{}.json?fields=id,name,level,displayName,coordinates,phoneNumber,email,contactPerson,openingDate,parent[id,name,parent[name,id]],shortName,code,created,lastUpdated"
+base_url = "https://hiskenya.org/api/29/organisationUnits/{}.json?fields=id,name,displayName,coordinates,phoneNumber,email,contactPerson,openingDate,parent[id,name,parent[name,id]],shortName,code,created,lastUpdated,geometry,level"
+
+base_url_2 = "https://histracker.health.go.ke/api/organisationUnits/{}.json"
+
+# base_url = "https://test.hiskenya.org/api/29/organisationUnits/{}.json?fields=id,name,level,displayName,coordinates,phoneNumber,email,contactPerson,openingDate,parent[id,name,parent[name,id]],shortName,code,created,lastUpdated"
 
 payload = {}
 headers = {
@@ -113,27 +98,53 @@ headers = {
     "Cookie": "JSESSIONID=800106A351A3EEE49B4C6223AA7F4822",
 }
 
+headers_2 = {
+    "Authorization": f"Basic {auth_coded_2}",
+    "Cookie": "JSESSIONID=800106A351A3EEE49B4C6223AA7F4822",
+}
+
 
 def fetchFacility(facilityuid):
     url = base_url.format(facilityuid)
+    print(f"Fetching facility {facilityuid} from {url}")
+    payload = {}
     try:
         response = requests.request("GET", url, headers=headers, data=payload)
-        if response.status_code == 200:
+        print(f"Response status code: {response}")
+        if response.status_code == 200 or response.status_code == 201:
+            print(f"Fetched facility {facilityuid} successfully")
             return response.json()
         else:
             return None
 
     except Exception as e:
         print(f"Error fetching facility {facilityuid}, Error is : {e}")
+        
+def fetchFacility_2(facilityuid):
+    url_2 = base_url_2.format(facilityuid)
+    print(f"Fetching facility {facilityuid} from {url_2}")
+    payload = {}
+    try:
+        # breakpoint()
+        response = requests.request("GET", url_2, headers=headers_2, data=payload)
+        print(f"Response status code: {response}")
+        if response.status_code == 200 or response.status_code == 201:
+            print(f"Fetched facility {facilityuid} successfully")
+            return response.json()
+        else:
+            return None
+
+    except Exception as e:
+        print(f"Error fetching facility in fetchFacility_2 {facilityuid}, Error is : {e}")
 
 
-base_create_url = "https://{}/api/organisationUnits"
+base_create_url = "https://{}/api/29/organisationUnits"
 
 update_base_create_url = "https://{}/api/organisationUnits/{}"
 
 
 # structure of system {"name":"","username": "","password": "", "url": ""}
-def createFacility(system, payload_send):
+def createFacility(system, payload_send, current_payload):
     p_username = os.getenv(f"{system['username']}", "")
     p_password = os.getenv(f"{system['password']}", "")
     post_credentials = f"{p_username}:{p_password}"
@@ -145,32 +156,40 @@ def createFacility(system, payload_send):
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "Content-Type": "application/json",
         "Connection": "keep-alive",
-        "Cookie": "JSESSIONID=B4C5226F366CCA42E45D57FB428D671E",
     }
 
     url = base_create_url.format(system["url"])
-    # print(f"Data being sent before edit: {payload_send}")
-    # url = update_base_create_url.format(system['url'],payload_send['id'])
+    print(f"Data being sent before edit: {payload_send}")
+    update_url = update_base_create_url.format(system['url'],payload_send['id'])
+    print(f"current_payload: {current_payload}")
+    print(f"update url: {update_url}")
 
     # modify payload dates
-    payload_send["openingDate"] = payload_send["openingDate"].split("T")[0]
-    payload_send["lastUpdated"] = payload_send["lastUpdated"].split("T")[0]
-    payload_send["created"] = payload_send["created"].split("T")[0]
+    if current_payload is not None and payload_send is not None:
+        current_payload["geometry"] = payload_send["geometry"]
+    else:
+        print(f"current_payload: {current_payload is None} or payload_send: {payload is None}, skipping geometry update")
+        exit(1)
+    # payload_send["lastUpdated"] = payload_send["lastUpdated"].split("T")[0]
+    # payload_send["created"] = payload_send["created"].split("T")[0]
 
     print(
-        f"{system['name']} Username: {p_username} - Password: {p_password}, url: {url}, encoded _password: {auth_coded_post}"
+        f"{system['name']} Username: {p_username} - Password: {p_password}, url: {update_url}, encoded _password: {auth_coded_post}"
     )
 
     print(f"Data being sent: {payload_send}")
 
     try:
         # Always change the line below to post or put
-        resp = requests.post(
-            url=url, data=json.dumps(payload_send), headers=p_headers, verify=False
-        )
-
-        # resp = requests.put(url=url, data=json.dumps(payload_send), headers=p_headers, verify=False)
-
+        # resp = requests.post(
+        #     url=url, data=json.dumps(payload_send), headers=p_headers, verify=False
+        # )
+        # if the org is in system, make an update
+        # if resp.status_code == 409:
+        #     print(f"Conflict detected, trying to update {system['name']} facility")
+        resp = requests.put(url=update_url, data=json.dumps(payload_send), headers=p_headers, verify=False)
+        print(f"Response status code: {resp.text}")
+        # breakpoint()
         if resp.status_code == 200 or resp.status_code == 201:
             print(f"Data posted Successfully")
             resp_json = resp.json()
@@ -184,9 +203,12 @@ def createFacility(system, payload_send):
 
 for facility in facilities:
     resp = fetchFacility(facility)
+    current_payload = fetchFacility_2(facility)
     if resp != None:
         for post_system in post_systems:
-            post_resp = createFacility(post_system, resp)
+            post_resp = createFacility(post_system, resp, current_payload)
             print(f"Posted facility: {post_resp}")
-        # if (post_resp['msg']=="Error"):
-        #     break
+    else:
+        print(f"Facility {facility} not found or could not be fetched.")
+        
+    # break
